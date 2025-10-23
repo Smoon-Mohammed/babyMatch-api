ee8c5bb1138eb3e5ac58c6e9bcdec8712bd9fb3d
Mohammed Tahri <mohammed.tahri24@gmail.com>
2025-10-23 21:07:35 +0200

    feat:first data user

--- src/main/resources/db/migration/V20251023160000__init.sql
+++ src/main/resources/db/migration/V20251023160000__init.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS users (
                                      id SERIAL PRIMARY KEY,
-                                     username VARCHAR(50) NOT NULL,
-                                     email VARCHAR(100) NOT NULL
+                                     username VARCHAR(50) NOT NULL UNIQUE,
+                                     email VARCHAR(100) NOT NULL UNIQUE,
+                                     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
